0d4ea8273b8415d16b66ca8f0069fbec6013c140
Rohit Singh <rkochhar@uwaterloo.ca>
2022-11-27 13:21:34 -0400

    added ReplyError and unit test

--- reed.go
+++ reed.go
@@ -1,6 +1,9 @@
 package reed
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 /*
 Name: ReplyTextContent
@@ -22,4 +25,27 @@ func ReplyTextContent(w http.ResponseWriter, r *http.Request, status int, conten
 	w.Header().Set("Content-Type", "text/plain") // Set the header to plain/text
 	w.WriteHeader(status)                        // Set the request status
 	w.Write([]byte(content + "\n"))              // Attach the message to the body
+}
+
+/*
+Name: ReplyError
+
+Description:
+  - Logs error message information to standard logger
+  - Sends error message back to request sender
+
+Inputs:
+  - w (http.ResponseWriter): Used to craft the HTTP response
+  - r (*http.Request): Used to access information about the initial request
+  - status (int): Specifies the HTTP response code
+  - errMessage (string): Text content to be included in the response body
+
+Outputs:
+  - None
+*/
+func ReplyError(w http.ResponseWriter, r *http.Request, status int, errMessage string) {
+	// Log the error to the standard logger
+	log.Printf("%s %s: Error %d %s", r.URL, r.Method, status, errMessage)
+	// Reply to the request with an error
+	http.Error(w, errMessage, status)
 }
--- reed_test.go
+++ reed_test.go
@@ -22,6 +22,53 @@ func setupAPI(t *testing.T, r *mux.Router) *httptest.Server {
 	return ts
 }
 
+// TestReplyError uses table-driven testing to make sure
+// the ReplyError function is working as expected
+func TestReplyError(t *testing.T) {
+	// Use table-driven testing to cover various edge cases
+	testCases := []struct {
+		name    string // Name of the test
+		status  int    // HTTP response code
+		content string // response body
+	}{
+		{"SimpleErrorMessage", http.StatusNotFound, "not lookin good"},
+	}
+	for _, tc := range testCases {
+		t.Run("Test"+tc.name, func(t *testing.T) {
+			// Define a function wrapper that will be used
+			// to handle get requests to the root
+			handler := func(w http.ResponseWriter, r *http.Request) {
+				reed.ReplyError(w, r, tc.status, tc.content)
+			}
+			// Attach the handler to the root of the server
+			r := mux.NewRouter()
+			r.HandleFunc("/", handler)
+			// Create a HTTP test server
+			ts := setupAPI(t, r)
+			defer ts.Close()
+			// Send a GET request to the root
+			resp, err := http.Get(ts.URL + "/")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp.StatusCode != tc.status {
+				t.Fatalf("Expected status code %s, instead got %s",
+					http.StatusText(tc.status),
+					http.StatusText(resp.StatusCode),
+				)
+			}
+			// Check that we have the content that we expected
+			var body []byte
+			if body, err = io.ReadAll(resp.Body); err != nil {
+				t.Fatal(err)
+			}
+			if !strings.Contains(string(body), tc.content) {
+				t.Fatalf("Expected %q, got %q.", tc.content, string(body))
+			}
+		})
+	}
+}
+
 // TestReplyTextContent uses table-driven testing to make sure
 // the ReplyTextContent function is working as expected
 func TestReplyTextContent(t *testing.T) {
